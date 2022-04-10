0bcbbbcab26d1814d53b7950ccdd1da211ea8009
somtom <som_tom@web.de>
2022-04-10 20:29:29 +0200

    Added class loggers and tests

--- logger.py
+++ logger.py
@@ -5,122 +5,168 @@ import pandas as pd
 from datetime import datetime, timedelta
 from config import DB_PATH
 
+
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger('youless')
 
 
-GRANULARITY_MAP = {
-    'minute': {'param': 'h', 'reports': 20, 'table': 'youless_minute'},
-    'hour': {'param': 'd', 'reports': 70, 'table': 'youless_hour'},
-    'day': {'param': 'm', 'reports': 12, 'table': 'youless_day'}
-}
+class YoulessBaseLogger:
+    GRANULARITY_MAP = {
+        'minute': {'param': 'h', 'reports': 20},
+        'hour': {'param': 'd', 'reports': 70},
+        'day': {'param': 'm', 'reports': 12}
+    }
+    con = None
+    cur = None
+    granularity = None
+    report_param = None
+    report_pages = None
+    table_name = None
+    chart_data = pd.DataFrame()
+    endpoint = 'http://youless/'
+    default_params =  {
+        'f': 'j'  # JSON response format,
+    }
 
-def convert_data(data):
-    res = []
-    time_format = '%Y-%m-%dT%H:%M:%S'
-    timestamp = datetime.strptime(data['tm'], time_format) 
-    for val in data['val']:
-        if val and val != '*':
-            res.append({
-                'time': timestamp,
-                'energy_consumption' : float(val.replace(',', '.')),
-                'unit': data['un']
-            })
-        timestamp += timedelta(seconds=data['dt'])
+    def __init__(self):
+        self.report_param = self.GRANULARITY_MAP[self.granularity]['param']
+        self.report_pages = self.GRANULARITY_MAP[self.granularity]['reports']
+        self.logger = logging.getLogger('Youless Scraper {}'.format(self.granularity))
+
+    @property
+    def youless_path(self) -> str:
+        raise NotImplementedError('youless_path attribute needs to be implemented in deriving class')
+
+    @property
+    def table_name(self) -> str:
+        raise NotImplementedError('table_name attribute needs to be implemented in deriving class')
+
+    @property
+    def granularity(self) -> str:
+        raise NotImplementedError('granularity attribute needs to be implemented in deriving class')
+
+    @property
+    def endpoint(self) -> str:
+        return f'http://youless/{self.youless_path}'
+
+    def fetch_data(self):
+        self.logger.info('Fetching new data for {} reports'.format(self.report_pages))
+        res = []
+        for page in range(self.report_pages):
+            data = requests.get(
+                self.endpoint, 
+                params={
+                    **self.default_params, 
+                    self.report_param: page + 1
+                }
+            )
+            res += YoulessBaseLogger.convert_data(data.json())
+        self.logger.info('Received {} entries'.format(len(res)))
+        self.store_data(pd.DataFrame(res))
+
+    @staticmethod
+    def convert_data(data: dict) -> list:
+        res = []
+        time_format = '%Y-%m-%dT%H:%M:%S'
+        timestamp = datetime.strptime(data['tm'], time_format) 
+        for val in data['val']:
+            if val and val != '*':
+                res.append({
+                    'time': timestamp,
+                    'energy_consumption' : float(val.replace(',', '.')),
+                    'unit': data['un']
+                })
+            timestamp += timedelta(seconds=data['dt'])
+            
+        return res
+
+    def table_exists(self) -> bool:
+        query = '''
+        SELECT name 
+        FROM sqlite_master 
+        WHERE 
+            type='table'
+            AND name='{table_name}'; 
+        '''
         
-    return res
+        with sql.connect(DB_PATH) as con:
+            cur = con.cursor()
+            listOfTables = cur.execute(query.format(table_name=self.table_name)).fetchall()
+        return len(listOfTables) > 0
+
+    def store_data(self, df):
+        with sql.connect(DB_PATH) as con:
+            if not self.table_exists(): 
+                self.logger.warning(f'Table {self.table_name} does not exist. Creating...')
+                # Just upload all data and create the table
+                df.to_sql(self.table_name, con, index=False)
+                value_count = len(df)
+            else:
+                # Store to temporary db
+                df.to_sql('tmp', con, if_exists='replace', index=False)
+
+                # Update existing data
+                query = '''
+                UPDATE {table_name} AS old
+                SET energy_consumption = (
+                    SELECT energy_consumption
+                    FROM tmp
+                    WHERE time = old.time
+                    LIMIT 1
+                ) 
+                WHERE EXISTS (
+                    SELECT energy_consumption
+                    FROM tmp
+                    WHERE time = old.time
+                )
+                '''
+                
+                cur = con.cursor()
+                cur.execute(query.format(table_name=self.table_name))
+                con.commit()
+                self.logger.info('Updated {} old values'.format(cur.rowcount))
+
+                # Store new data
+                query = '''
+                INSERT INTO {table_name} (time, energy_consumption, unit)
+                SELECT time, energy_consumption, unit
+                FROM tmp AS new
+                WHERE NOT EXISTS (
+                    SELECT 1 FROM {table_name} old 
+                    WHERE old.time = new.time
+                );
+                '''
+                
+                cur = con.cursor()
+                cur.execute(query.format(table_name=self.table_name))
+                con.commit()
+                value_count = cur.rowcount
+            
+            logger.info('Uploaded {} new values'.format(value_count))
 
 
-def fetch_data(granularity):
-    param = GRANULARITY_MAP[granularity]['param']
-    reports = GRANULARITY_MAP[granularity]['reports']
-
-    logger.info('Fetching new data. Granularity: {}, reports: {}'.format(granularity, reports))
-    res = []
-    for i in range(reports):
-        data = requests.get('http://youless/V?{param}={page}&f=j'.format(
-            param=param,
-            page=i+1
-        )).json()
-        res += convert_data(data)
-    logger.info('Received {} entries'.format(len(res)))
-    return pd.DataFrame(res)
+class YoulessEnergyMinute(YoulessBaseLogger):
+    youless_path = 'V'
+    table_name = 'youless_minute'
+    granularity = 'minute'
 
 
-def table_exists(table, cur):
-    query = '''
-    SELECT name 
-    FROM sqlite_master 
-    WHERE 
-        type='table'
-        AND name='{table_name}'; 
-    '''
-    listOfTables = cur.execute(query.format(table_name=table)).fetchall()
-    return len(listOfTables) > 0
+class YoulessEnergyHour(YoulessBaseLogger):
+    youless_path = 'V'
+    table_name = 'youless_hour'
+    granularity = 'hour'
 
 
-def store_data(df, con):
-    cur = con.cursor()
-    table = GRANULARITY_MAP[granularity]['table']
-
-    if not table_exists(table, cur): 
-        logger.warning('Table {} does not exist. Creating...'.format(table))
-        # Just upload all data and create the table
-        df.to_sql(table, con, index=False)
-        value_count = len(df)
-    else:
-        # Store to temporary db
-        df.to_sql('tmp', con, if_exists='replace', index=False)
-
-        # Update existing data
-        query = '''
-        UPDATE {table_name} AS old
-        SET energy_consumption = (
-            SELECT energy_consumption
-            FROM tmp
-            WHERE time = old.time
-            LIMIT 1
-        ) 
-        WHERE EXISTS (
-            SELECT energy_consumption
-            FROM tmp
-            WHERE time = old.time
-        )
-        '''
-        cur.execute(query.format(table_name=table))
-        con.commit()
-        logger.info('Updated {} old values'.format(cur.rowcount))
-
-        # Store new data
-        query = '''
-        INSERT INTO {table_name} (time, energy_consumption, unit)
-        SELECT time, energy_consumption, unit
-        FROM tmp AS new
-        WHERE NOT EXISTS (
-            SELECT 1 FROM {table_name} old 
-            WHERE old.time = new.time
-        );
-        '''
-        cur = con.cursor()
-        cur.execute(query.format(table_name=table))
-        con.commit()
-        value_count = cur.rowcount
-    
-    logger.info('Uploaded {} new values'.format(value_count))
-
+class YoulessEnergyDay(YoulessBaseLogger):
+    youless_path = 'V'
+    table_name = 'youless_day'
+    granularity = 'day'
 
 
 if __name__ == '__main__':
-    granularity = 'minute'
-    with sql.connect(DB_PATH) as con:
-        granularity = 'minute'
-        df = fetch_data(granularity)
-        store_data(df, con)
-
-        granularity = 'hour'
-        df = fetch_data(granularity)
-        store_data(df, con)
-
-        granularity = 'day'
-        df = fetch_data(granularity)
-        store_data(df, con)
+    energy_minute_scraper = YoulessEnergyMinute()
+    energy_minute_scraper.fetch_data()
+    energy_hour_scraper = YoulessEnergyHour()
+    energy_hour_scraper.fetch_data()
+    energy_day_scraper = YoulessEnergyDay()
+    energy_day_scraper.fetch_data()
---
+++ test/test_logger.py
@@ -0,0 +1,56 @@
+import datetime
+
+from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
+from logger import YoulessBaseLogger
+
+
+class TestScraper(YoulessBaseLogger):
+    youless_path = 'test_path'
+    granularity = 'day'
+    table_name = 'test_table'
+
+
+class FetchDataTestCase(TestCase):
+    @patch('logger.YoulessBaseLogger.store_data')
+    @patch('logger.YoulessBaseLogger.convert_data')
+    @patch('logger.requests.get')
+    def test_fetching_for_day_granularity(self, mocked_get, mocked_convert_data, mocked_store_data):
+        """
+        ... then the correct endpoint should have been called 12 times with correct parameters
+        """
+        data = {'un':'Watt','tm':'2022-04-10T00:00:00','dt':3600,'val':[' 27',' 33',' 27',' 33',' 27',' 33',' 27',' 26',' 74',' 53',' 107',' 86']}
+        mocked_get.return_value = MagicMock(json=lambda: data)
+        converted_data = YoulessBaseLogger.convert_data(data)
+        mocked_convert_data.return_value = converted_data
+
+        scraper = TestScraper()
+        scraper.fetch_data()
+        
+        self.assertEqual(mocked_get.call_count, 12)
+        mocked_store_data.assert_called_once()
+        
+
+
+class ConvertDataTestCase(TestCase):
+    @patch('logger.requests.get')
+    def test_data_provided(self, mocked_get):
+        """
+        ... then the data should be properly reformatted
+        """
+        data = {'un':'Watt','tm':'2022-04-10T00:00:00','dt':3600,'val':[' 27,1',' 33']}
+        expected_timestamp = datetime.datetime(2022, 4, 10)
+        expected_energy_consumption = [27.1, 33]
+        excpected_unit = data['un']
+
+        scraper = TestScraper()
+        res = scraper.convert_data(data)
+        first_row = res[0]
+        energy_consumption = [e['energy_consumption'] for e in res]
+
+        self.assertEqual(len(res), len(data['val']))
+        self.assertEqual(first_row['time'], expected_timestamp)
+        self.assertEqual(first_row['unit'], excpected_unit)
+        self.assertCountEqual(energy_consumption, expected_energy_consumption)
+        
